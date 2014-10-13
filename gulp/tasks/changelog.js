505ffbaaa50fd3c33ba7ff3ac9f9e0c1935e5498
thaiat <thaiat@ipelia.fr>
2014-10-13 00:43:46 +0000

    feat(git): Add validation for commit messages

--- gulp/tasks/changelog.js
+++ gulp/tasks/changelog.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 var changelog = require('conventional-changelog');
 var argv = require('yargs').argv;
 var marked = require('marked');
@@ -9,8 +10,11 @@ var q = require('q');
 var pkg = require('../../package.json');
 var path = require('path');
 var gutil = require('gulp-util');
+var exec = $.exec;
+var concat = $.concat;
+var constants = require('../common/constants')();
 
-var repository = 'https://github.com/thaiat/generator-angular-famous-ionic';
+var repository = constants.repository;
 if(repository.length <= 0) {
     throw new Error('The repository cannot be empty');
 }
@@ -39,7 +43,7 @@ var makeChangelog = function(options) {
     return deferred.promise;
 };
 
-gulp.task('changelog', function() {
+gulp.task('changelog:conventional', function() {
     var dest = argv.dest || 'CHANGELOG.md';
     var toHtml = argv.html || false;
     return makeChangelog(argv).then(function(log) {
@@ -51,3 +55,24 @@ gulp.task('changelog', function() {
         fs.writeFileSync(dest, log);
     });
 });
+
+gulp.task('changelog:script', function() {
+    var options = argv;
+    var version = options.version || pkg.version;
+    var from = options.from || '';
+    var streamqueue = require('streamqueue');
+    var stream = streamqueue({
+        objectMode: true
+    });
+
+    stream.queue(gulp.src('').pipe(exec('node ./gulp/common/changelog-script.js ' + version + ' ' + from, {
+        pipeStdout: true
+    })));
+    stream.queue(gulp.src('CHANGELOG.md'));
+
+    return stream.done()
+        .pipe(concat('CHANGELOG.md'))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('changelog', ['changelog:script']);
