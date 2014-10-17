ce8e4f958e73f5dd0da1ce13f954807f087d1171
thaiat <thaiat@ipelia.fr>
2014-10-17 23:12:49 +0000

    feat(app): Add scaffolding for route
    Fixes #22

--- templates/module/index.js
+++ templates/module/index.js
@@ -1,4 +1,5 @@
 'use strict';
+require('angular-ui-router');
 <% if (ionic) { %>require('angular-ionic');<% } %>
 <% if (famous) { %>require('famous-angular');<% } %>
 
@@ -9,8 +10,16 @@ module.exports = function(namespace) {
     var fullname = namespace + '.' + modulename;
 
     var angular = require('angular');
-    var app = angular.module(fullname, [<%= ngModules %>]);
+    var app = angular.module(fullname, ['ui.router', <%= ngModules %>]);
     // inject:folders start
     // inject:folders end
+    
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider.state('home', {
+            url : '/',
+            template: require('./views/home.html')
+       });                                  
+    }]);
     return app;
 };
