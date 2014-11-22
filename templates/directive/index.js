6903082f8b2177f2242dbfd3f34d7e06369d6829
thaiat <thaiat@ipelia.fr>
2014-11-22 14:28:08 +0000

    feat(directive): Add better support for controllerAs syntax
    Fixes #73

--- templates/directive/index.js
+++ templates/directive/index.js
@@ -14,14 +14,15 @@ module.exports = function(app) {
     var directiveDeps = [];
     var directive = function() {
         return {
-            restrict: 'AEC',
-            replace: true,
+            restrict: 'AE',
             scope: {
                 title: '@' // '@' reads attribute value, '=' provides 2-way binding, '&" works with functions
             },
             controller: controller,
+            controllerAs: 'ctrl',
+            bindToController: true,
             template: require('./<%= directivename %>.html'),
-            link: function($scope, element, attrs) {
+            link: function(scope, element, attrs) {
 
             }
         };
