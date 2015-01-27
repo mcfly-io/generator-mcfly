4c9738aa4ae235570998d0131075e7934af1ca96
Avi Haiat <thaiat@ipelia.fr>
2015-01-27 13:30:07 +0200

    feat(directive): Use unitHelper in directives tests

--- templates/directive/index.test.js
+++ templates/directive/index.test.js
@@ -3,20 +3,13 @@
 var angular = require('angular-mocks');
 var app = require('../')('app');
 var directivename = '<%= directivename %>';
+var unitHelper = require('unitHelper');
+
 describe(app.name, function() {
 
     describe('Directives', function() {
 
         describe(directivename, function() {
-
-            var compileDirective = function(html) {
-                var element = angular.element(html);
-                this.$compile(element)(this.$scope);
-                this.$scope.$digest();
-                this.controller = element.controller(directivename);
-                this.scope = element.isolateScope() || element.scope();
-                return element;
-            };
 
             beforeEach(function() {
                 angular.mock.module(app.name);
@@ -26,12 +19,11 @@ describe(app.name, function() {
                 this.$templateCache = $injector.get('$templateCache');
                 this.$compile = $injector.get('$compile');
                 this.$scope = $injector.get('$rootScope').$new();
-                this.$scope.vm = {};
             }));
 
             it('should succeed', function() {
-                var element = compileDirective.call(this, '<<%= htmlname %>></<%= htmlname %>>');
-                expect(element.html().trim()).toBe('This is directive : <%= htmlname %>');
+                var element = unitHelper.compileDirective.call(this, '<<%= htmlname %>></<%= htmlname %>>');
+                expect(element.html().trim()).toBeDefined();
             });
 
         });
