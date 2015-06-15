9c2d4c57623135e2c8c79ea34bd847e56e132c74
Avi Haiat <thaiat@ipelia.fr>
2015-06-15 21:21:33 +0300

    feat(app): Add deviceready for mobile app not using ionic

--- templates/target/scripts/main.js
+++ templates/target/scripts/main.js
@@ -26,6 +26,21 @@ var run = function($ionicPlatform, $window) {
         }
     });
 };
+<% } else if (mobile && !ionic) { %>
+var runDeps = ['$window'];
+var run = function($window) {
+    document.addEventListener('deviceready', function () {
+        if($window.cordova && $window.cordova.plugins.Keyboard) {
+            $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if($window.StatusBar) {
+            $window.StatusBar.styleDefault();
+        }
+        if($window.TestFairy) {
+            $window.TestFairy.begin('');
+        }
+    }, false);
+}
 <% } else { %>
 var runDeps = [];
 var run = function() {
