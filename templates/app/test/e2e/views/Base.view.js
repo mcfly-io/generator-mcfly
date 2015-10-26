d13694e5437ea9b1971c7a7e5730a9a281961cbd
Avi Haiat <thaiat@ipelia.fr>
2015-10-26 20:13:52 +0200

    fix(e2e): Simplify base view get function

--- templates/app/test/e2e/views/Base.view.js
+++ templates/app/test/e2e/views/Base.view.js
@@ -10,14 +10,7 @@ class BaseView {
     }
 
     get() {
-        return browser.getCurrentUrl().then(currentUrl => {
-            //console.log('browse.get ' + 'from ' + currentUrl + ' to ' + this.url);
-            //if (!_.endsWith(currentUrl, this.url)) {
-            return browser.get(this.url).then(() => {
-                return browser.waitForUrlToChangeTo(new RegExp(this.url + '$'));
-            });
-            //}
-        });
+        return browser.get(this.url);
     }
 }
 
